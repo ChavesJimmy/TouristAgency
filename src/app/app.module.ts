ffb8282088c3342e5026c019d1dc5950f2e108fa
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-10 10:52:15 +0200

    create app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
